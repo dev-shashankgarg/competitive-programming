5cd7bdc435ce6b14732f4fd3889ef91f6f5dc36c
dev-shashankgarg <shashankgarg.tu@gmail.com>
2021-05-05 19:45:40 +0530

    local sync - 05/05/2021

---
+++ codechef/src/main/java/contest/lunchapril21b/CCHeaven.java
@@ -0,0 +1,181 @@
+package contest.lunchapril21b;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.util.stream.IntStream;
+
+public class CCHeaven {
+
+  public static void main(String[] args) {
+    Print print = new Print();
+    Scan scan = new Scan();
+
+    int t = scan.scanInt();
+    IntStream.range(0, t).forEach(test -> {
+      int n = scan.scanInt();
+      boolean flag = false;
+      char[] arr = scan.scanString().toCharArray();
+      int co = 0;
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == '1') {
+          co++;
+        }
+        int total = i + 1;
+        int exp = (total + 1) / 2;
+        if (co >= exp) {
+          flag = true;
+          break;
+        }
+      }
+
+      print.printLine(flag ? "YES" : "NO");
+
+    });
+
+    print.close();
+
+  }
+
+  static class Scan {
+
+    private byte[] buf = new byte[1024];
+    private int index;
+    private InputStream in;
+    private int total;
+
+    public Scan() {
+      in = System.in;
+    }
+
+    public int scan() {
+      if (total < 0) {
+        throw new InputMismatchException();
+      }
+      if (index >= total) {
+        index = 0;
+        try {
+          total = in.read(buf);
+        } catch (IOException ignored) {
+        }
+        if (total <= 0) {
+          return -1;
+        }
+      }
+      return buf[index++];
+    }
+
+    public int scanInt() {
+      int integer = 0;
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      int neg = 1;
+      if (n == '-') {
+        neg = -1;
+        n = scan();
+      }
+      while (!isWhiteSpace(n)) {
+        if (n >= '0' && n <= '9') {
+          integer *= 10;
+          integer += n - '0';
+          n = scan();
+        } else {
+          throw new InputMismatchException();
+        }
+      }
+      return neg * integer;
+    }
+
+    public double scanDouble() {
+      double doub = 0;
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      int neg = 1;
+      if (n == '-') {
+        neg = -1;
+        n = scan();
+      }
+      while (!isWhiteSpace(n) && n != '.') {
+        if (n >= '0' && n <= '9') {
+          doub *= 10;
+          doub += n - '0';
+          n = scan();
+        } else {
+          throw new InputMismatchException();
+        }
+      }
+      if (n == '.') {
+        n = scan();
+        double temp = 1;
+        while (!isWhiteSpace(n)) {
+          if (n >= '0' && n <= '9') {
+            temp /= 10;
+            doub += (n - '0') * temp;
+            n = scan();
+          } else {
+            throw new InputMismatchException();
+          }
+        }
+      }
+      return doub * neg;
+    }
+
+    public String scanString() {
+      StringBuilder sb = new StringBuilder();
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      while (!isWhiteSpace(n)) {
+        sb.append((char) n);
+        n = scan();
+      }
+      return sb.toString();
+    }
+
+    private boolean isWhiteSpace(int n) {
+      if (n == '\n' || n == '\r' || n == '\t' || n == -1) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  static class Print {
+
+    private final BufferedWriter bw;
+
+    public Print() {
+      bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    public void print(String str) {
+      try {
+        bw.append(str);
+      } catch (IOException ignored) {
+      }
+    }
+
+    public void printLine(String str) {
+      print(str);
+      try {
+        bw.append("\n");
+      } catch (IOException ignored) {
+      }
+    }
+
+    public void close() {
+      try {
+        bw.close();
+      } catch (IOException ignored) {
+      }
+    }
+  }
+
+}
