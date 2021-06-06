f0d4614f54b44d9c9e774412be95b68473384152
dev-shashankgarg <shashankgarg.tu@gmail.com>
2021-06-06 20:07:30 +0530

    local sync - 06/06/2021

---
+++ codeforces/src/main/java/contest/educational/n110/B.java
@@ -0,0 +1,333 @@
+package contest.educational.n110;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+
+public class B {
+
+  public static void main(String[] args) {
+    Print print = new Print();
+    Scan scan = new Scan();
+
+    int t = scan.scanInt();
+    IntStream.range(0, t).forEach(test -> {
+      int n = scan.scanInt();
+      Integer[] data = scan.scan1dIntArray();
+
+      long val = ((long) n * n - n) / 2;
+      print.printLine(Long.toString(val - solve(data)));
+
+    });
+
+    print.close();
+
+  }
+
+  private static long solve(Integer[] data) {
+    long ans = 0;
+    for (int i = 0; i < data.length; i++) {
+      for (int j = i + 1; j < data.length; j++) {
+        if (gcd(data[i], data[j] * 2) == 1 && gcd(data[i] * 2, data[j]) == 1) {
+
+          ans++;
+        }
+      }
+    }
+
+    return ans;
+  }
+
+  private static long gcd(long a, long b) {
+    if (b == 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  }
+
+  static class Scan {
+
+    private byte[] buf = new byte[1024];
+    private int index;
+    private InputStream in;
+    private int total;
+
+    public Scan() {
+      in = System.in;
+    }
+
+    public int scan() {
+      if (total < 0) {
+        throw new InputMismatchException();
+      }
+      if (index >= total) {
+        index = 0;
+        try {
+          total = in.read(buf);
+        } catch (IOException ignored) {
+        }
+        if (total <= 0) {
+          return -1;
+        }
+      }
+      return buf[index++];
+    }
+
+    public int scanInt() {
+      int integer = 0;
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      int neg = 1;
+      if (n == '-') {
+        neg = -1;
+        n = scan();
+      }
+      while (!isWhiteSpace(n)) {
+        if (n >= '0' && n <= '9') {
+          integer *= 10;
+          integer += n - '0';
+          n = scan();
+        } else {
+          throw new InputMismatchException();
+        }
+      }
+      return neg * integer;
+    }
+
+    public double scanDouble() {
+      double doub = 0;
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      int neg = 1;
+      if (n == '-') {
+        neg = -1;
+        n = scan();
+      }
+      while (!isWhiteSpace(n) && n != '.') {
+        if (n >= '0' && n <= '9') {
+          doub *= 10;
+          doub += n - '0';
+          n = scan();
+        } else {
+          throw new InputMismatchException();
+        }
+      }
+      if (n == '.') {
+        n = scan();
+        double temp = 1;
+        while (!isWhiteSpace(n)) {
+          if (n >= '0' && n <= '9') {
+            temp /= 10;
+            doub += (n - '0') * temp;
+            n = scan();
+          } else {
+            throw new InputMismatchException();
+          }
+        }
+      }
+      return doub * neg;
+    }
+
+    public Integer[] scan1dIntArray() {
+      String[] s = this.scanString().split(" ");
+      Integer[] arr = new Integer[s.length];
+      for (int i = 0; i < s.length; i++) {
+        arr[i] = Integer.parseInt(s[i]);
+      }
+      return arr;
+    }
+
+    public Integer[][] scan2dIntArray(int n, int m) {
+      Integer[][] arr = new Integer[n][m];
+      for (int i = 0; i < n; i++) {
+        String[] s = this.scanString().split(" ");
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = Integer.parseInt(s[j]);
+        }
+      }
+      return arr;
+    }
+
+    public String[] scan1dStringArray() {
+      return this.scanString().split(" ");
+    }
+
+    public String[][] scan2dStringArray(int n, int m) {
+      String[][] arr = new String[n][m];
+      for (int i = 0; i < n; i++) {
+        String[] s = this.scanString().split(" ");
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = s[j];
+        }
+      }
+      return arr;
+    }
+
+    public char[] scan1dCharArray() {
+      return this.scanString().toCharArray();
+    }
+
+    public char[][] scan2dCharArray(int n, int m) {
+      char[][] arr = new char[n][m];
+      for (int i = 0; i < n; i++) {
+        char[] s = this.scanString().toCharArray();
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = s[j];
+        }
+      }
+      return arr;
+    }
+
+    public Long[] scan1dLongArray() {
+      String[] s = this.scanString().split(" ");
+      Long[] arr = new Long[s.length];
+      for (int i = 0; i < s.length; i++) {
+        arr[i] = Long.parseLong(s[i]);
+      }
+      return arr;
+    }
+
+    public Long[][] scan2dLongArray(int n, int m) {
+      Long[][] arr = new Long[n][m];
+      for (int i = 0; i < n; i++) {
+        String[] s = this.scanString().split(" ");
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = Long.parseLong(s[j]);
+        }
+      }
+      return arr;
+    }
+
+    public String scanString() {
+      StringBuilder sb = new StringBuilder();
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      while (!isWhiteSpace(n)) {
+        sb.append((char) n);
+        n = scan();
+      }
+      return sb.toString();
+    }
+
+    private boolean isWhiteSpace(int n) {
+      if (n == '\n' || n == '\r' || n == '\t' || n == -1) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  static class Print {
+
+    private final BufferedWriter bw;
+
+    public Print() {
+      bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    public void print(String str) {
+      try {
+        bw.append(str);
+      } catch (IOException ignored) {
+      }
+    }
+
+    public void printLine(Integer[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (Integer x : arr) {
+        sj.add(Integer.toString(x));
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(Integer[][] arr) {
+      for (Integer[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (Integer y : x) {
+          sj.add(Integer.toString(y));
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(String[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (String x : arr) {
+        sj.add(x);
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(String[][] arr) {
+      for (String[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (String y : x) {
+          sj.add(y);
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(char[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (char x : arr) {
+        sj.add(Character.toString(x));
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(char[][] arr) {
+      for (char[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (char y : x) {
+          sj.add(Character.toString(y));
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(Long[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (Long x : arr) {
+        sj.add(Long.toString(x));
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(Long[][] arr) {
+      for (Long[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (Long y : x) {
+          sj.add(Long.toString(y));
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(String str) {
+      print(str);
+      try {
+        bw.append("\n");
+      } catch (IOException ignored) {
+      }
+    }
+
+    public void close() {
+      try {
+        bw.close();
+      } catch (IOException ignored) {
+      }
+    }
+  }
+
+}
---
+++ codeforces/src/main/java/contest/educational/n110/C.java
@@ -0,0 +1,399 @@
+package contest.educational.n110;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+
+public class C {
+
+  public static void main(String[] args) {
+    Print print = new Print();
+    Scan scan = new Scan();
+
+    int t = scan.scanInt();
+    IntStream.range(0, t).forEach(test -> {
+      char[] data = scan.scan1dCharArray();
+      print.printLine(Long.toString(solve(data)));
+    });
+
+    print.close();
+
+  }
+
+  private static long solve(char[] data) {
+    int i = 0;
+    int j = 0;
+    List<Node> li = new ArrayList<>();
+
+    while (j < data.length) {
+      if (data[j] == '?') {
+        if (i != j) {
+          li.add(new Node(data[i], data[j - 1], j - i));
+          i = j;
+        }
+        while (j < data.length && data[j] == '?') {
+          j++;
+        }
+        li.add(new Node(data[i], data[j - 1], j - i));
+        i = j;
+      } else if (i == j || data[j] != data[j - 1]) {
+        j++;
+      } else {
+        li.add(new Node(data[i], data[j - 1], j - i));
+        i = j;
+      }
+    }
+
+    if (i != data.length) {
+      li.add(new Node(data[i], data[j - 1], j - i));
+    }
+
+    long ans = 0;
+    for (int k = 0; k < li.size(); k++) {
+      if (li.get(k).l == '?') {
+        if (k == 0) {
+          if (li.size() == 1) {
+            long n = li.get(k).size;
+            return (n * (n + 1)) / 2;
+          } else {
+            li.get(k + 1).size += li.get(k).size;
+          }
+        } else if (k == li.size() - 1) {
+          li.get(k - 1).size += li.get(k).size;
+        } else {
+          long n = li.get(k).size;
+          if ((n % 2 == 1 && li.get(k - 1).r == li.get(k + 1).l) ||
+              (n % 2 == 0 && li.get(k - 1).r != li.get(k + 1).l)) {
+            li.get(k + 1).size += (li.get(k - 1).size + li.get(k).size);
+            li.get(k - 1).size = 0;
+          } else {
+            li.get(k - 1).size += (li.get(k).size);
+            li.get(k + 1).size += (li.get(k).size);
+
+            long nn = li.get(k).size;
+            ans -= ((nn * (nn + 1)) / 2);
+          }
+        }
+      }
+    }
+
+    for (int k = 0; k < li.size(); k++) {
+      if (li.get(k).l != '?') {
+        long n = li.get(k).size;
+        ans += ((n * (n + 1)) / 2);
+      }
+    }
+
+    return ans;
+  }
+
+  static class Node {
+
+    char l;
+    char r;
+    int size;
+
+    public Node(char l, char r, int size) {
+      this.l = l;
+      this.r = r;
+      this.size = size;
+    }
+
+    @Override
+    public String toString() {
+      return "Node{" +
+          "l=" + l +
+          ", r=" + r +
+          ", size=" + size +
+          '}';
+    }
+  }
+
+  static class Scan {
+
+    private byte[] buf = new byte[1024];
+    private int index;
+    private InputStream in;
+    private int total;
+
+    public Scan() {
+      in = System.in;
+    }
+
+    public int scan() {
+      if (total < 0) {
+        throw new InputMismatchException();
+      }
+      if (index >= total) {
+        index = 0;
+        try {
+          total = in.read(buf);
+        } catch (IOException ignored) {
+        }
+        if (total <= 0) {
+          return -1;
+        }
+      }
+      return buf[index++];
+    }
+
+    public int scanInt() {
+      int integer = 0;
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      int neg = 1;
+      if (n == '-') {
+        neg = -1;
+        n = scan();
+      }
+      while (!isWhiteSpace(n)) {
+        if (n >= '0' && n <= '9') {
+          integer *= 10;
+          integer += n - '0';
+          n = scan();
+        } else {
+          throw new InputMismatchException();
+        }
+      }
+      return neg * integer;
+    }
+
+    public double scanDouble() {
+      double doub = 0;
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      int neg = 1;
+      if (n == '-') {
+        neg = -1;
+        n = scan();
+      }
+      while (!isWhiteSpace(n) && n != '.') {
+        if (n >= '0' && n <= '9') {
+          doub *= 10;
+          doub += n - '0';
+          n = scan();
+        } else {
+          throw new InputMismatchException();
+        }
+      }
+      if (n == '.') {
+        n = scan();
+        double temp = 1;
+        while (!isWhiteSpace(n)) {
+          if (n >= '0' && n <= '9') {
+            temp /= 10;
+            doub += (n - '0') * temp;
+            n = scan();
+          } else {
+            throw new InputMismatchException();
+          }
+        }
+      }
+      return doub * neg;
+    }
+
+    public Integer[] scan1dIntArray() {
+      String[] s = this.scanString().split(" ");
+      Integer[] arr = new Integer[s.length];
+      for (int i = 0; i < s.length; i++) {
+        arr[i] = Integer.parseInt(s[i]);
+      }
+      return arr;
+    }
+
+    public Integer[][] scan2dIntArray(int n, int m) {
+      Integer[][] arr = new Integer[n][m];
+      for (int i = 0; i < n; i++) {
+        String[] s = this.scanString().split(" ");
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = Integer.parseInt(s[j]);
+        }
+      }
+      return arr;
+    }
+
+    public String[] scan1dStringArray() {
+      return this.scanString().split(" ");
+    }
+
+    public String[][] scan2dStringArray(int n, int m) {
+      String[][] arr = new String[n][m];
+      for (int i = 0; i < n; i++) {
+        String[] s = this.scanString().split(" ");
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = s[j];
+        }
+      }
+      return arr;
+    }
+
+    public char[] scan1dCharArray() {
+      return this.scanString().toCharArray();
+    }
+
+    public char[][] scan2dCharArray(int n, int m) {
+      char[][] arr = new char[n][m];
+      for (int i = 0; i < n; i++) {
+        char[] s = this.scanString().toCharArray();
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = s[j];
+        }
+      }
+      return arr;
+    }
+
+    public Long[] scan1dLongArray() {
+      String[] s = this.scanString().split(" ");
+      Long[] arr = new Long[s.length];
+      for (int i = 0; i < s.length; i++) {
+        arr[i] = Long.parseLong(s[i]);
+      }
+      return arr;
+    }
+
+    public Long[][] scan2dLongArray(int n, int m) {
+      Long[][] arr = new Long[n][m];
+      for (int i = 0; i < n; i++) {
+        String[] s = this.scanString().split(" ");
+        for (int j = 0; j < m; j++) {
+          arr[i][j] = Long.parseLong(s[j]);
+        }
+      }
+      return arr;
+    }
+
+    public String scanString() {
+      StringBuilder sb = new StringBuilder();
+      int n = scan();
+      while (isWhiteSpace(n)) {
+        n = scan();
+      }
+      while (!isWhiteSpace(n)) {
+        sb.append((char) n);
+        n = scan();
+      }
+      return sb.toString();
+    }
+
+    private boolean isWhiteSpace(int n) {
+      if (n == '\n' || n == '\r' || n == '\t' || n == -1) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  static class Print {
+
+    private final BufferedWriter bw;
+
+    public Print() {
+      bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    public void print(String str) {
+      try {
+        bw.append(str);
+      } catch (IOException ignored) {
+      }
+    }
+
+    public void printLine(Integer[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (Integer x : arr) {
+        sj.add(Integer.toString(x));
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(Integer[][] arr) {
+      for (Integer[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (Integer y : x) {
+          sj.add(Integer.toString(y));
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(String[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (String x : arr) {
+        sj.add(x);
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(String[][] arr) {
+      for (String[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (String y : x) {
+          sj.add(y);
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(char[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (char x : arr) {
+        sj.add(Character.toString(x));
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(char[][] arr) {
+      for (char[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (char y : x) {
+          sj.add(Character.toString(y));
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(Long[] arr) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (Long x : arr) {
+        sj.add(Long.toString(x));
+      }
+      printLine(sj.toString());
+    }
+
+    public void printLine(Long[][] arr) {
+      for (Long[] x : arr) {
+        StringJoiner sj = new StringJoiner(" ");
+        for (Long y : x) {
+          sj.add(Long.toString(y));
+        }
+        printLine(sj.toString());
+      }
+    }
+
+    public void printLine(String str) {
+      print(str);
+      try {
+        bw.append("\n");
+      } catch (IOException ignored) {
+      }
+    }
+
+    public void close() {
+      try {
+        bw.close();
+      } catch (IOException ignored) {
+      }
+    }
+  }
+
+}
